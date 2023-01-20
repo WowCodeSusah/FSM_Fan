71986adb4e9b34901a827ce7cbbe53e270610af6
WowCodeSusah <114371708+WowCodeSusah@users.noreply.github.com>
2023-01-20 14:23:24 +0700

    Update mylib.c

--- mylib/mylib.c
+++ mylib/mylib.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-void state(int a, int b, int c, int d, int* S0, int* S1) {
+void state(int a, int b, int c, int* S0, int* S1) {
 // state0 = a, state1 = b, output0 = c, output1 = d
-    int e = !a & c & !d;
-    int f = !b & c & d;
-    int g = b & !c & d;
-    int h = a & !c & !d;
+    int e = !a & !b & !c;
+    int f = a & b & !c;
+    int g = !a & b & c;
+    int h = a & !b & c;
     *S0 = e | f | g | h;
-    int i = !a & !b & d;
-    int j = b & !d;
-    int k = a & !d;
-    *S1 = i | j | k;
+    *S1 = !b;
 }
 
 void output(int s0, int s1, int* o0, int* o1) {
